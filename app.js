5e9d8be24a693a97a140eea65cdb3eefe042449f
Elena Hadjimina <hadjimina.elena@gmail.com>
2020-12-19 20:59:09 +0800

    added stylesheet

--- app.js
+++ app.js
@@ -32,4 +32,13 @@ app.get('/beers', (req, res) => {
     .catch(error => console.log(error));
 });
 
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('random-beers', { beerslist: randomBeer });
+    })
+    .catch(error => console.log(error));
+});
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
